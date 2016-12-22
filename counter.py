b45cc05fc84219fc6a136b5650290af161ef0097
gspracklin <spracklin@wisc.edu>
2016-12-22 10:57:04 -0500

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,18 @@
+usr0 = input("What file (SAM format only) :")
+f = open(usr0, 'r')
+w = open(usr0 + '_oma-1', 'w')
+usr3 = input("What chromosome? :")
+usr1 = int(input("What is the starting position? :"))
+usr2 = int(input("What is the ending position? :"))
+
+
+total_reads = 0
+for line in f:
+    total_reads += 1
+    parts = line.split()
+    if len(parts) > 4: #removes header from analysis
+        if len(parts[9]) < 30: # analyzes reads short than 30bp
+            if parts[2] == usr3: #Chromosome selection
+                if int(parts[3]) >= usr1 and int(parts[3]) < usr2: #basepair number
+                    w.write(line)
+print("Total reads,", total_reads)
